acf2196e7afca5c9f2cb27eb85e2d1ebc537abcc
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-01-31 20:41:01 +0700

    Добавлены пользователи, фильтрация

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import { useState } from "react";
+import { useEffect } from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState('');
+  useEffect(() => {
+    fetch("https://reqres.in/api/users")
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении данных о пользователях");
+      }).finally(()=>setIsLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (event) => {
+    setSearchValue(event.target.value);
+  }
+
   return (
     <div className="App">
-      <Users />
+      <Users onChangeSearchValue={onChangeSearchValue} searchValue={searchValue} items={users} isLoading={isLoading}/>
       {/* <Success /> */}
     </div>
   );
