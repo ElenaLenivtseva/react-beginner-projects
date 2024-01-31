84c900729587859577e32c0c4fc22167c665eeb5
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-01-31 18:46:23 +0700

    Компонент Modal изменен: добавила возможность помещать контент внутрь модального окна. Также в стилях была неточность с высотой modal, из-за которой все, что превышало 300px не помещалось в блок modal. Исправлено

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { useState } from 'react';
 import './index.scss';
 
-const Modal = ({open, setOpen}) => (
+const Modal = ({open, setOpen, children}) => (
 <div className={`overlay animated ${open ? 'show': ''}`}>
         <div className="modal">
           <svg height="200" viewBox="0 0 200 200" width="200" onClick={() => setOpen(false)}>
             <title />
             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
-          <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='for fun'/>
+          {children}
         </div>
       </div>
 )
@@ -19,7 +19,10 @@ function App() {
   return (
     <div className="App">
       <button className="open-modal-btn" onClick={() => setOpen(true)}>✨ Открыть окно</button>
-      <Modal open={open} setOpen={setOpen}/>
+      <Modal open={open} setOpen={setOpen}>
+        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='for fun'/>
+        <p>wow</p>
+      </Modal>
       
     </div>
   );
