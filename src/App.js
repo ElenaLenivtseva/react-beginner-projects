fe9027c59b836dc2fe320b322513e5ae19d1ea29
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-01-31 18:27:18 +0700

    Модальное окно с анимацией. Удалила функции, т.к. в теле разметки есть вычисления да и короче так

--- src/App.js
+++ src/App.js
@@ -4,24 +4,19 @@ import './index.scss';
 
 function App() {
   const [open, setOpen] = useState(false)
-  function handleClickOpen() {
-    setOpen(true)
-  }
-  function handleClickClose() {
-    setOpen(false)
-  }
+  
   return (
     <div className="App">
-      <button className="open-modal-btn" onClick={handleClickOpen}>✨ Открыть окно</button>
-      {open && <div className="overlay">
+      <button className="open-modal-btn" onClick={() => setOpen(true)}>✨ Открыть окно</button>
+      <div className={`overlay animated ${open ? 'show': ''}`}>
         <div className="modal">
-          <svg height="200" viewBox="0 0 200 200" width="200" onClick={handleClickClose}>
+          <svg height="200" viewBox="0 0 200 200" width="200" onClick={() => setOpen(false)}>
             <title />
             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
           </svg>
           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" alt='for fun'/>
         </div>
-      </div>}
+      </div>
       
     </div>
   );
