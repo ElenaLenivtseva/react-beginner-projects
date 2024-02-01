485b9f396bd4a40b4ca73a9078a176a9e6656884
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-02-01 23:58:09 +0700

    Создана галерея. Стоит доработать категории - лучше бы и их мы запрашивали с сервера. Достаточно 1 раз при загрузке, как мне кажется. А то получилась смесь. Также не очень хорошо, что mockAPI не знает макс количество страниц, но на это мы повлиять не можем. Может быть, стоит передавать лимит, убрать hardcode значение

--- src/App.js
+++ src/App.js
@@ -1,49 +1,65 @@
-import React from 'react';
-import './index.scss';
+import React from "react";
+import {useState, useEffect} from "react";
+import { Collection } from "./Collection.jsx";
+import "./index.scss";
 
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
-
+const categories = [
+  { name: "Все" },
+  { name: "Море" },
+  { name: "Горы" },
+  { name: "Архитектура" },
+  { name: "Города" },
+];
 function App() {
+  const [categoryId, setCategoryId] = useState(0);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(true);
+  const [searhValue, setSearchValue] = useState("");
+  const [collections, setCollections] = useState([]);
+ 
+  useEffect(() => {
+    setIsLoading(true);
+    const category = categoryId ? `category=${categoryId}` : ''; 
+    
+    fetch(`https://65bbb77d52189914b5bcee99.mockapi.io/collections?page=${page}&limit=3&${category}`)
+      .then((res) => res.json())
+      .then((json) => {
+        if (json) {
+          setCollections(json);
+        }       
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении данных");
+      }).finally(()=> setIsLoading(false));
+  }, [categoryId, page ]);
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+          {categories.map((obj, idx) => (
+            <li onClick={()=>setCategoryId(idx)} className={categoryId === idx ? "active" : ""} key={obj.name}>
+              {obj.name}
+            </li>
+          ))}
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
-      </div>
-      <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
+        <input
+          value={searhValue}
+          onChange={(e) => setSearchValue(e.target.value)}
+          className="search-input"
+          placeholder="Поиск по названию"
         />
       </div>
+      <div className="content">
+        {isLoading ? (<h3>Идет загрузка...</h3>) : (collections.filter(obj => obj.name.toLowerCase().includes(searhValue.toLowerCase())).map((obj, index)=><Collection key={index} name={obj.name} images={obj.photos}/>))}
+        
+      </div>
       <ul className="pagination">
-        <li>1</li>
-        <li className="active">2</li>
-        <li>3</li>
+        {/* MockAPI не может посчитать макс кол-во страниц сам, поэтому hardcode */}
+        {
+          [...Array(5)].map((_, idx)=> <li onClick={()=>setPage(idx+1)} className={page===idx+1 ? 'active' : ''}>{idx+1}</li>)
+        }
       </ul>
     </div>
   );
