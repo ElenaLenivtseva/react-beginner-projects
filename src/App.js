f449cddf04094375fd93b185d836d03da2ce03f5
ElenaLenivtseva <lenivtseva.lena@yandex.ru>
2024-02-02 09:46:13 +0700

    Теперь категории также получаются с сервера. Не стала делать isloading в этом Эффекте, т.к. боюсь последствий установки true/false из 2 мест, а в один Эффект это объяединять не стоит, потому что это разные процессы. Коллекция фото явно тяжелее, чем просто список категорий, поэтому разумно, если isloading будет зависеть от коллекции фото

--- src/App.js
+++ src/App.js
@@ -1,45 +1,59 @@
 import React from "react";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { Collection } from "./Collection.jsx";
 import "./index.scss";
 
-const categories = [
-  { name: "Все" },
-  { name: "Море" },
-  { name: "Горы" },
-  { name: "Архитектура" },
-  { name: "Города" },
-];
 function App() {
   const [categoryId, setCategoryId] = useState(0);
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(true);
   const [searhValue, setSearchValue] = useState("");
   const [collections, setCollections] = useState([]);
- 
+  const [categories, setCategories] = useState([]);
+
   useEffect(() => {
-    setIsLoading(true);
-    const category = categoryId ? `category=${categoryId}` : ''; 
-    
-    fetch(`https://65bbb77d52189914b5bcee99.mockapi.io/collections?page=${page}&limit=3&${category}`)
+    fetch(`https://65bbb77d52189914b5bcee99.mockapi.io/categories`)
       .then((res) => res.json())
       .then((json) => {
         if (json) {
-          setCollections(json);
-        }       
+          setCategories(json);
+        }
       })
       .catch((err) => {
         console.warn(err);
         alert("Ошибка при получении данных");
-      }).finally(()=> setIsLoading(false));
-  }, [categoryId, page ]);
+      });
+  }, []);
+  useEffect(() => {
+    setIsLoading(true);
+    const category = categoryId ? `category=${categoryId}` : "";
+
+    fetch(
+      `https://65bbb77d52189914b5bcee99.mockapi.io/collections?page=${page}&limit=3&${category}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        if (json) {
+          setCollections(json);
+        }
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("Ошибка при получении данных");
+      })
+      .finally(() => setIsLoading(false));
+  }, [categoryId, page]);
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
           {categories.map((obj, idx) => (
-            <li onClick={()=>setCategoryId(idx)} className={categoryId === idx ? "active" : ""} key={obj.name}>
+            <li
+              onClick={() => setCategoryId(idx)}
+              className={categoryId === idx ? "active" : ""}
+              key={obj.name}
+            >
               {obj.name}
             </li>
           ))}
@@ -52,14 +66,28 @@ function App() {
         />
       </div>
       <div className="content">
-        {isLoading ? (<h3>Идет загрузка...</h3>) : (collections.filter(obj => obj.name.toLowerCase().includes(searhValue.toLowerCase())).map((obj, index)=><Collection key={index} name={obj.name} images={obj.photos}/>))}
-        
+        {isLoading ? (
+          <h3>Идет загрузка...</h3>
+        ) : (
+          collections
+            .filter((obj) =>
+              obj.name.toLowerCase().includes(searhValue.toLowerCase())
+            )
+            .map((obj, index) => (
+              <Collection key={index} name={obj.name} images={obj.photos} />
+            ))
+        )}
       </div>
       <ul className="pagination">
         {/* MockAPI не может посчитать макс кол-во страниц сам, поэтому hardcode */}
-        {
-          [...Array(5)].map((_, idx)=> <li onClick={()=>setPage(idx+1)} className={page===idx+1 ? 'active' : ''}>{idx+1}</li>)
-        }
+        {[...Array(5)].map((_, idx) => (
+          <li
+            onClick={() => setPage(idx + 1)}
+            className={page === idx + 1 ? "active" : ""}
+          >
+            {idx + 1}
+          </li>
+        ))}
       </ul>
     </div>
   );
